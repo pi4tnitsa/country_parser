0b1fc9dd4c04e60c4ac925b5c6646637da1756d8
pi4tnitsa <148749899+pi4tnitsa@users.noreply.github.com>
2025-04-11 18:36:39 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# config.py
+
+# Bot configuration
+BOT_TOKEN = ''
+PASSWORD = ''
+
+# Database configuration
+DB_NAME = ''
+DB_USER = ''
+DB_PASSWORD = ''  # if you add password for db
+DB_HOST = ''
+DB_PORT = ''
